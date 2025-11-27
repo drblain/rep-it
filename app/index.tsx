af147ead1e9c1ee7590e42f478193a7c02f6ffaf
drblain <drblain22@gmail.com>
2025-11-27 09:53:53 -0500

    Fix tailwind config stuff (finally)

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function Index() {
+  const insets = useSafeAreaInsets();
+
   return (
     <View className="flex-1 bg-background items-center justify-center">
-      <Text className="text-red text-xl font-bold">Tailwind is working!</Text>
+      <Text className="text-foreground text-xl font-bold">
+        Tailwind is working!
+      </Text>
     </View>
   );
 }
